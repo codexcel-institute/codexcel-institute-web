3bfbcbf250a8d87183a73cd25a96398819ab805e
0147ntekim <147ntekim@gmail.com>
2024-07-04 12:37:48 +0100

    commit

--- src/SecondaryPageFolder/Offer.jsx
+++ src/SecondaryPageFolder/Offer.jsx
@@ -15,15 +15,15 @@ const Offer = () => {
                     <h1 className="text-[30px] gochi font-semibold capitalize">relaxation</h1>
                     <p className="text-[14px] ">By having your ecommerce and customer data in one central location, you can connect with your audience in a whole new way. Context matters.</p>
                 </div>
-                <div className="flex flex-col justify-between py-[60px] px-[24px] text-[black] hover:text-[white] hover:bg-[black] hover:rounded-xl">
+                <div className="flex flex-col justify-between py-[50px] px-[24px] text-[black] hover:text-[white] hover:bg-[black] hover:rounded-xl">
                     <h1 className="text-[30px] gochi font-semibold capitalize">home</h1>
                     <p className="text-[14px] ">More than just card retries. From abandoned carts to failed payment recovery, understand where your leaky buckets are and how to fix them.</p>
                 </div>
-                <div className="flex flex-col justify-between py-[60px] px-[25px] text-[black] hover:text-[white] hover:bg-[black] hover:rounded-xl">
+                <div className="flex flex-col justify-between py-[50px] px-[25px] text-[black] hover:text-[white] hover:bg-[black] hover:rounded-xl">
                     <h1 className="text-[30px] gochi font-semibold capitalize">rest</h1>
                     <p className="text-[14px] ">Frequently communicating with your audience is half the battle. Learn how your message is being received so you can course correct quickly.</p>
                 </div>
-                <div className="flex flex-col justify-between py-[60px] px-[25px] text-[black] hover:text-[white] hover:bg-[black] hover:rounded-xl">
+                <div className="flex flex-col justify-between py-[50px] px-[25px] text-[black] hover:text-[white] hover:bg-[black] hover:rounded-xl">
                     <h1 className="text-[30px] gochi font-semibold  capitalize">security</h1>
                     <p className="text-[14px] ">Have peace of mind monitoring your most valuable metrics relating to your business. From MRR, refunds, average order value, and more.</p>
                 </div>
