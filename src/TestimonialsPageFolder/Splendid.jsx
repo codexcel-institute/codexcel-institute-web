18df972a40601a33efa63c327af4538e7df34dd6
Akinola Kehinde <khennyakin@gmail.com>
2024-07-11 17:29:38 +0100

    third page

--- src/TestimonialsPageFolder/Splendid.jsx
+++ src/TestimonialsPageFolder/Splendid.jsx
@@ -7,7 +7,7 @@ function Splendid() {
         <div className='w-5/12 flex items-center justify-center'>
             <div className="w-10/12 flex flex-col gap-4">
                 <p>START YOUR CAREER IN TECH</p>
-                <h1 className="gochi text-6xl">BEST INSTITUTE EVER</h1>
+                <h1 className="gochi text-6xl">SPLENDID COURSES</h1>
                 <p>This is where dreams come to life. With our carefully crafted learning courses we will meet you where you are, and take you to where you want to be in your career.</p>
             </div>
         </div>
--- src/TestimonialsPageFolder/WhoWeAre.jsx
+++ src/TestimonialsPageFolder/WhoWeAre.jsx
@@ -9,7 +9,7 @@ function WhoWeAre() {
         <div className='w-5/12 flex items-center justify-center'>
             <div className="w-10/12 flex flex-col gap-4">
                 <p>START YOUR CAREER IN TECH</p>
-                <h1 className="gochi text-6xl">BEST INSTITUTE EVER</h1>
+                <h1 className="gochi text-6xl">WHO WE ARE</h1>
                 <p>This is where dreams come to life. With our carefully crafted learning courses we will meet you where you are, and take you to where you want to be in your career.</p>
             </div>
         </div>
