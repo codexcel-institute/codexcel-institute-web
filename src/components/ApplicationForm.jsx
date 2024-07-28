2258f68ece0ce8fa8766fbeda2751ae7053d9c6c
Akinola Kehinde <khennyakin@gmail.com>
2024-07-28 10:25:13 +0100

    feat: added a feature to receive applicants data

--- src/components/ApplicationForm.jsx
+++ src/components/ApplicationForm.jsx
@@ -1,18 +1,47 @@
-import { useContext, useState } from 'react'
+import { useContext, useRef, useState } from 'react'
 import { FormContext } from './Context'
 import { IoMdClose } from 'react-icons/io'
+import emailjs from '@emailjs/browser';
 
 function ApplicationForm() {
+
     const { displayForm, closeForm } = useContext(FormContext)
 
     
-    const [selectedCourse, setSelectedCourse] = useState('Select Course')
+    const [selectedCourse, setSelectedCourse] = useState('')
 
     const courses = ["Web Development", "UI/UX", "Data Science", "Cyber Security"]
     const [displayCourses, setDisplayCourses] = useState(false)
+
+    function resetForm(){
+        const form = document.querySelector('#form')
+        form.reset()
+        setSelectedCourse('')
+    }
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_jayy97k', 'template_wa35nlm', form.current, {
+        publicKey: 'ClYPUAtkrpd9RKHfY',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          resetForm()
+          closeForm()
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <section className={`fixed z-[999] flex items-center justify-center ${displayForm ? 'top-0' : 'top-[-100vh]'} left-0 w-full h-screen backdrop-brightness-75`} onClick={() => closeForm()}>
-        <form id='form' action="" className={`bg-white border border-[rgb(229,225,218)] w-10/12 max-w-[450px] px-5 rounded-[4px] py-4 overflow-y-scroll h-screen max-h-[600px] ${displayForm ? 'scale-100 transition-all duration-300 delay-300 ease-in-out' : 'scale-0'}`} onClick={(e) => {
+        <form ref={form} onSubmit={sendEmail} id='form' action="" className={`bg-white border border-[rgb(229,225,218)] w-10/12 max-w-[450px] px-5 rounded-[4px] py-4 overflow-y-scroll h-screen max-h-[600px] ${displayForm ? 'scale-100 transition-all duration-300 delay-300 ease-in-out' : 'scale-0'}`} onClick={(e) => {
             e.stopPropagation()
             setDisplayCourses(false)
         }}>
@@ -20,17 +49,17 @@ function ApplicationForm() {
             <h1 className="text- text-xl mb-4 text-center px-10">Register To Start Your Career In Tech</h1>
             <div className='mb-4'>
                 <label htmlFor="" className="block font-bold mb-1">Firstname</label>
-                <input type="text" name="" id="" className="w-full border border-[rgba(229,225,218,1)] px-4 rounded-[4px] h-11" />
+                <input type="text" name="firstname" id="" className="w-full border border-[rgba(229,225,218,1)] px-4 rounded-[4px] h-11" />
             </div>
 
             <div className='mb-4'>
                 <label htmlFor="" className="block font-bold mb-1">Lastname</label>
-                <input type="text" name="" id="" className="w-full border border-[rgba(229,225,218,1)] px-4 rounded-[4px] h-11" />
+                <input type="text" name="lastname" id="" className="w-full border border-[rgba(229,225,218,1)] px-4 rounded-[4px] h-11" />
             </div>
 
             <div className='mb-4'>
                 <label htmlFor="" className="block font-bold mb-1">Phone</label>
-                <input type="text" name="" id="" className="w-full border border-[rgba(229,225,218,1)] px-4 rounded-[4px] h-11" />
+                <input type="text" name="phone" id="" className="w-full border border-[rgba(229,225,218,1)] px-4 rounded-[4px] h-11" />
             </div>
 
             <div className='mb-4 relative'>
@@ -39,7 +68,7 @@ function ApplicationForm() {
                     e.stopPropagation()
                     setDisplayCourses(prev => !prev)
                 }}>
-                    <div>{selectedCourse}</div>
+                    <div>{selectedCourse ? selectedCourse : 'Select Course'}</div>
                     <div className={`absolute left-0 top-[44px] bg-white shadow-xl w-full rounded-md ${displayCourses ? 'h-[150px] duration-300 delay-150 transition-all ease-in-out' : 'h-0' } overflow-hidden`}>
                         {
                             courses.map((course, i) => (
@@ -49,17 +78,13 @@ function ApplicationForm() {
                             ))
                         }
                     </div>
-                    {/* <option className="hover:bg-black" value="">select Course</option>
-                    <option className="hover:bg-black" value="web development">Web Development</option>
-                    <option className="hover:bg-black" value="Product Design">UI/UX</option>
-                    <option className="hover:bg-black" value="">Data Science</option>
-                    <option className="hover:bg-black" value="">Cyber Security</option> */}
+                    <input type="text" name="course" id="" value={selectedCourse} className='hidden' />
                 </div>
             </div>
 
             <div className='mb-4'>
                 <label htmlFor="" className="block font-bold mb-1">Email Address</label>
-                <input type="text" name="" id="" className="w-full border border-[rgba(229,225,218,1)] px-4 rounded-[4px] h-11" />
+                <input type="text" name="email" id="" className="w-full border border-[rgba(229,225,218,1)] px-4 rounded-[4px] h-11" />
             </div>
 
             <button className="w-full h-11 bg-[rgba(3,239,98,1)] rounded-[4px] font-bold">Submit</button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import {useContext, useState} from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import logo from '../assets/codexlogo.png'
 import Links from './Menu'
@@ -20,14 +20,14 @@ function Navbar() {
       <nav className="h-[80px] bg-white text-black inter flex  items-center justify-between pl-[3%] pr-[3%] md:pr-[4%] relative">
         <span onClick={toggleNav} className='text-[25px] block md:hidden' id="openHam">&#9776;</span>
         <Link to="/"><img className='logo mr-[270px] md:mr-0' src={logo} alt="logo" /></Link>
-        <ul className='flex items-center justify-between w-auto md:w-[580px]'>
+        <ul className='flex items-center justify-between w-auto md:w-[75%] lg:w-[70%]'>
           <ul className='hidden md:flex items-center justify-between w-[330px] md:w-[400px]'>
             <li className='nav-link'><NavLink to="for-secondary-schools">For Schools</NavLink></li>
             <li className='nav-link'><NavLink to="testimonials">Testimonials</NavLink></li>
             <li className='nav-link'><NavLink to="faqs">FAQ</NavLink></li>
             <li className='nav-link'><NavLink to="community">Community</NavLink></li>
           </ul>
-          <li><button className="bg-[rgba(52,168,83,1)] text-white w-36 h-12 rounded-md font-semibold" onClick={() => openForm()}>Apply Now</button></li>
+          <li><button className="bg-[rgba(52,168,83,1)] text-white ml-auto w-36 h-12 rounded-md font-semibold" onClick={() => openForm()}>Apply Now</button></li>
         </ul>
 
 
