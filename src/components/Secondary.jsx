72c75763618098e4b8f7a5aa6b35b5eff00ded20
Akinola Kehinde <khennyakin@gmail.com>
2024-08-16 17:22:01 +0100

    feat: added a link to take user to our whatsapp

--- src/components/Secondary.jsx
+++ src/components/Secondary.jsx
@@ -8,7 +8,7 @@ function Secondary() {
 
         <h1 className='text-6xl gochi max-w-[730px]'>Codexcel for secondary school curriculum</h1>
         <p className='max-w-[570px] mb-3'>Learn the data and AI skills you need online at your own pace—from non-coding essentials to data science, AI, and machine learning.</p>
-        <button className="capitalize rounded-[100px] bg-[rgba(0,255,68,1)] w-[200px] md:w-[217px] h-[60px] md:h-[70px] hover:scale-110 text-2xl ">Learn more</button>
+        <a href='https://wa.me/2348184902178'><button className="capitalize rounded-[100px] bg-[rgba(0,255,68,1)] w-[200px] md:w-[217px] h-[60px] md:h-[70px] hover:scale-110 text-2xl ">Learn more</button></a>
     </section>
   )
 }
