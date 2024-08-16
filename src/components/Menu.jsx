ed0c3e1e3857184517ffe10e4998ef19af027a69
Akinola Kehinde <khennyakin@gmail.com>
2024-08-16 15:06:18 +0100

    testing mobile nav

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -25,7 +25,7 @@ export default function Links({ displayMenu, setDisplayMenu }){
 
     return(
 
-        <div className={`nav-menu-parent fixed flex pt-[100px] justify-center top-0 left-0 w-full ${displayMenu ? 'block' : 'hidden'} h-svh`} onClick={closeMenu}>
+        <div className={`nav-menu-parent fixed flex pt-[100px] justify-center top-0 left-0 w-full ${displayMenu ? 'block' : 'hidden'} h-screen bg-red-500`} onClick={closeMenu}>
             <ul className={`child  rounded-3xl flex flex-col md:hidden items-center justify-between py-[80px] w-[90%] md:w-[0] h-[60vh] sm:h-[70vh] md:h-[0] z-[1] bg-[white] ${displayMenu ? 'block' : 'hidden'}`} onClick={(e) => e.stopPropagation()}>
                 <li className="gochi text-[30px]" onClick={closeMenu}><NavLink to="for-secondary-schools">For Schools</NavLink></li>
                 <li className="gochi text-[30px]" onClick={closeMenu}><NavLink to="testimonials">Testimonials</NavLink></li>
