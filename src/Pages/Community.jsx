5af08a3d321d307b5432b1dc9f2f4e5deaf06705
Akinola Kehinde <khennyakin@gmail.com>
2024-08-26 22:46:18 +0100

    added a bounce animation to faq and community pages

--- src/Pages/Community.jsx
+++ src/Pages/Community.jsx
@@ -2,8 +2,11 @@ import {} from 'react'
 
 function Community() {
   return (
-    <main className="h-[80vh] flex items-center justify-center text-9xl gochi">
-        Coming Soon...
+    <main className="h-[80vh] flex items-center justify-center text-5xl md:text-9xl gochi">
+        Coming Soon 
+          <span className="dot w-2 h-2 md:w-4 md:h-4 rounded-full bg-black"></span> 
+          <span className="dot w-2 h-2 md:w-4 md:h-4 rounded-full bg-black"></span> 
+          <span className="dot w-2 h-2 md:w-4 md:h-4 rounded-full bg-black"></span>
     </main>
   )
 }
